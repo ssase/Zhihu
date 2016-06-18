a71384a84c2e282f57ae334e0d97833b5cf8a06f
张敏 <zmtt@outlook.com>
2016-06-18 13:39:21 +0800

    Initial Commit

---
+++ Zhihu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Zhihu
+//
+//  Created by SASE on 6/18/16.
+//  Copyright © 2016 SASE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Zhihu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Zhihu
+//
+//  Created by SASE on 6/18/16.
+//  Copyright © 2016 SASE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
